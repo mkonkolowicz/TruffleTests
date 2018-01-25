d4bd7b580afd3b11a33b2206a8fa5b5b71b68363
mkonkolowicz <MaciekKonkolowicz@titlesource.com>
2018-01-25 17:32:32 -0500

    corrected import statements in testgreeter.sol

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -5,15 +5,15 @@ import "./Mortal.sol";
 
 contract greeter is mortal {
  /* define variable greeting of the type string */
-    string greeting;
+    bytes32 greeting;
  /* this runs when the contract is executed */
     
-    function greeter(string _greeting) public {
+    function greeter(bytes32 _greeting) public {
         greeting = _greeting;
     }
 
  /* main function */
-    function greet() constant returns (string) {
+    function greet() constant returns (bytes32) {
         return greeting;
     }
 }
--- test/TestGreeter.sol
+++ test/TestGreeter.sol
@@ -1,14 +1,15 @@
-import "../truffle/Assert.sol";
-import "../truffle/DeployedAddresses.sol";
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
 import "../contracts/Greeter.sol";
 
 
 contract TestGreeter {
     
     function testReply () {
-        var greeting = "Block Chains Gotta Block";
-        Greeter greeter = Greeter(DeployedAddresses.Greeter(greeting));
-        Assert.equal(greeter.greet(), greeting, "Greeting should be: "+greeting);
+        bytes32 greeting = "Block Chains Gotta Block";
+        greeter welcomer = greeter(DeployedAddresses.greeter());
+        bytes32 returnedGreeting = welcomer.greet(); 
+        Assert.equal(returnedGreeting, greeting, "Greeting should not be different");
     }
 
 }
